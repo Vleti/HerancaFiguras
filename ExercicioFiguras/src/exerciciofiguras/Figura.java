a1e184aa9459f813ccc7a021e5824a71fb671b90
Vleti <89584794+Vleti@users.noreply.github.com>
2021-12-08 10:33:07 -0300

    Att

---
+++ ExercicioFiguras/src/exerciciofiguras/Figura.java
@@ -0,0 +1,29 @@
+package exerciciofiguras;
+
+public abstract class Figura {
+    
+    int a=2;
+    public void draw(){
+        System.out.println("Desenhando...");
+    }
+     
+    public abstract void drawShape();
+    
+    
+    
+    
+    
+    
+    
+    
+    /*private String Figura="";
+
+    
+    public void setNomeFigura(String Figura) {
+        this.Figura = Figura;
+    }
+    
+     public String getNomeFigura() {
+        return Figura;*/
+    
+}
---
+++ ExercicioFiguras/src/exerciciofiguras/Triangulo.java
@@ -0,0 +1,10 @@
+
+package exerciciofiguras;
+
+
+public class Triangulo extends Figura {
+    
+    public void drawShape() {
+        System.out.println("Desenhando triangulo...");
+    }
+}
